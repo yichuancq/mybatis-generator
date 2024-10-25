eaaceaaa2601e6ae2ef906e3f9b3ba3589b0adb3
yichuan <1012027293@qq.com>
2024-10-25 22:38:31 +0800

    oracel12 c对应的数据库代码生成器配置,实体带备注

---
+++ src/main/java/com/example/gen/MyBatisGeneratorRun.java
@@ -0,0 +1,43 @@
+package com.example.gen;
+
+/**
+ * @author yichuan
+ * @version 1.0
+ * @description: MyBatisGeneratorRun
+ * @date 2024/10/25 22:19
+ */
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyBatisGeneratorRun {
+    public static void main(String[] args) throws Exception {
+        MyBatisGeneratorRun app = new MyBatisGeneratorRun();
+
+        System.out.println(app.getClass().getResource("/").getPath());
+        app.generator();
+        System.out.println(System.getProperty("user.dir"));
+    }
+
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(resourceAsStream);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+}
